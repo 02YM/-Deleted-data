d207fa3026fa38c5aa247bd387bcd124d1cee26e
02YM <dydals4289@gmail.com>
2025-05-13 19:29:39 +0900

    Vector + Quaternion
    Vector + Quaternion

--- Assets/EndAttackReset.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class EndAttackReset : StateMachineBehaviour
-{
-    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        animator.SetBool("SpecialSkill 0", false);
-    }
-}
